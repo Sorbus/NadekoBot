99c4b648fcf073c4c0bafe8a021a8d1b1c832a60
Else <Sorbus@users.noreply.github.com>
2016-12-01 20:02:10 -0800

    Added some extensions and modified ignore

---
+++ src/NadekoBot/_Extensions/Prose.cs
@@ -0,0 +1,99 @@
+﻿using Discord;
+using Discord.WebSocket;
+using ImageProcessorCore;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NadekoBot.Extensions
+{
+    public static class Prose
+    {
+        public static string CapitalizeFirst(this string s)
+        {
+            bool IsNewSentense = true;
+            var result = new StringBuilder(s.Length);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (IsNewSentense && char.IsLetter(s[i]))
+                {
+                    result.Append(char.ToUpper(s[i]));
+                    IsNewSentense = false;
+                }
+                else
+                    result.Append(s[i]);
+
+                if (s[i] == '!' || s[i] == '?' || s[i] == '.')
+                {
+                    IsNewSentense = true;
+                }
+            }
+
+            return result.ToString();
+        }
+
+        // from https://stackoverflow.com/a/2730393
+
+        public static string ToWords(this int number)
+        {
+            return ((long)number).ToWords();
+        }
+
+        public static string ToWords(this long number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + Math.Abs(number).ToWords();
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += (number / 1000000).ToWords() + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += (number / 1000).ToWords() + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += (number / 100).ToWords() + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
+    }
+}
